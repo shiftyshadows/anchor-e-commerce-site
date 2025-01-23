50363100076d8c7a4b4da8e45fb716827c926904
shiftyshadows <purple.quasar@proton.me>
2025-01-23 11:19:56 +0300

    Corrected ESLint errors

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,21 +3,35 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser', // Use Babel parser
   parserOptions: {
+    requireConfigFile: false,
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, // Enable JSX
     },
-    ecmaVersion: 12,
+    ecmaVersion: 12, // Support ES12
     sourceType: 'module',
+    babelOptions: {
+      presets: ['@babel/preset-env', '@babel/preset-react'], // Babel presets
+    },
   },
-  plugins: ['react'],
+  extends: [
+    'eslint:recommended', // Base recommended rules
+    'plugin:react/recommended', // React rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'airbnb', // Airbnb style guide
+    'prettier', // Disable ESLint rules conflicting with Prettier
+  ],
+  plugins: ['react', 'react-hooks', 'babel'], // Plugins for React and Babel
   rules: {
-    // Add your custom rules here
+    'react/react-in-jsx-scope': 'off', // Suppress error for React in new JSX
+    'react/prop-types': 'off', // Disable prop-types if using TypeScript
+    'babel/semi': 'error', // Ensure semicolons with Babel
+    'babel/no-invalid-this': 'error', // Prevent invalid `this` usage
   },
   settings: {
     react: {
-      version: 'detect', // Automatically detect React version
+      version: 'detect', // Auto-detect React version
     },
   },
 };
